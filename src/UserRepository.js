b7668925ed85dea77b0766b604b1458a156df895
Irmak Erdem <irmakerdem9@gmail.com>
2022-05-16 17:37:11 -0600

    Add file name to gitignore and create constructorfor UserRepository file and test

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,23 @@
 class UserRepository {
-
+    constructor(allUserData) {
+        this.data = allUserData;
+        //allUserData is an array
+    }
+    getUserDataBasedOnId(allUserData) {
+        // allUserData.find((userId) => userId === allUserData.id {
+        //     console.log(userId)
+        // })
+        // const user = new User()
+        //make instance of user that finds user based on id property
+        //.find()
+        //allUserData.id
+    }
+    getAveStepGoalOfAllUsers() {
+        //this.data.reduce()
+        //.toFixed() to round down to 2 decimals
+    }
 }
+// holds onto all of User objects
+// data parameter is user data
 
 export default UserRepository;
