d37fc6b0e3717f88661d983920b39dbc5572366b
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-05-18 11:33:58 -0600

    Add getRandomUser function and remove quotation mark from HTML

--- src/scripts.js
+++ src/scripts.js
@@ -10,6 +10,23 @@ import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
+// function getRandomElement(array) {
+//   return array[Math.floor(Math.random()*array.length)]
+// };
+// QUERY SELECTORS
+var allUserDataCard = document.querySelector('#userData');
+console.log(allUserDataCard)
+
+// EVENT LISTENERS
+// window.addEventListener('load', )
+
+// FUNCTIONS
+
+function getRandomUser(userData) {
+  return userData[Math.floor(Math.random()*userData.length)]
+};
+
+//
 // An example of how you tell webpack to use a JS file
 
 import userData from './data/users';
