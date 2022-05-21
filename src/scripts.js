30f47455cbe81e29a2732c7ed97673dcc71bf83a
Irmak Erdem <irmakerdem9@gmail.com>
2022-05-21 11:13:10 -0600

    Reorganize scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,61 +1,19 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// console.log(userData,"<>>>>userData")
+//IMPORTS
+// An example of how you tell webpack to use a JS file
+import UserRepository from './UserRepository';
+import Hydration from './Hydration';
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
-
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
 
-// console.log('This is the JavaScript entry file - your code begins here.');
-
-//API FETCH
-function getAllUserData(data) {
-      // console.log(users)
-      globalUserData = data;
-      globalUserRepository = new UserRepository(data);
-      // console.log(users.userData)
-      getUserName();
-
-}
-
-function getAllHydrationData(data) {
-
-  globalHydrationData = data;
-  // console.log(globalHydrationData)
-  globalHydration = new Hydration(data);
-}
-
-function getAllData() {
-
-  let foo = ['https://fitlit-api.herokuapp.com/api/v1/users', 'https://fitlit-api.herokuapp.com/api/v1/hydration',
-  'https://fitlit-api.herokuapp.com/api/v1/activity', 'https://fitlit-api.herokuapp.com/api/v1/sleep'];
-  let fooArr = foo.map((url) => {
-    return fetch(url)
-    .then(res => res.json())
-    .catch(error => console.log("fetch call error", error))
-  });
-
-
-
-  Promise.all(fooArr).then((value) => {
-    console.log(value);
-    getAllHydrationData(value[1].hydrationData);
-    getAllUserData(value[0].userData);
-    console.log(value[2])
-  });
-
-}
-
-// GLOBAL VARIABLES
+//GLOBAL VARIABLES
 let globalUserRepository;
 let globalUserData;
 let globalHydrationData;
 let globalHydration;
 
-
-// QUERY SELECTORS
+//QUERY SELECTORS
 var welcomeText = document.querySelector('#welcomeText');
 var idText = document.querySelector('#id');
 var nameText = document.querySelector('#name');
@@ -69,14 +27,43 @@ var averageUsersStepGoal = document.querySelector('#averageUsersStepGoal');
 var todaysHydration = document.querySelector('#todaysHydration');
 var weeklyHydration = document.querySelector('#weeklyHydration');
 
-// EVENT LISTENERS
-window.addEventListener('load', function() {
-  // getAllUserData()
-  // getAllHydrationData()
-  getAllData()
-});
+//EVENT LISTENERS
+window.addEventListener('load', fetchAllData);
 
-// FUNCTIONS
+//API FETCH
+function getAllUserData(data) {
+  // console.log(users)
+  globalUserData = data;
+  globalUserRepository = new UserRepository(data);
+  // console.log(users.userData)
+}
+
+function getAllHydrationData(data) {
+  globalHydrationData = data;
+  // console.log(globalHydrationData)
+  globalHydration = new Hydration(data);
+  getUserName();
+}
+
+function fetchAllData() {
+  let apis = ['https://fitlit-api.herokuapp.com/api/v1/users', 'https://fitlit-api.herokuapp.com/api/v1/hydration',
+  'https://fitlit-api.herokuapp.com/api/v1/activity', 'https://fitlit-api.herokuapp.com/api/v1/sleep'];
+  
+  let endpoints = apis.map((url) => {
+    return fetch(url)
+    .then(res => res.json())
+    .catch(error => console.log("fetch call error", error));
+  });
+
+  Promise.all(endpoints).then((value) => {
+    // console.log(value);
+    getAllUserData(value[0].userData);
+    getAllHydrationData(value[1].hydrationData);
+    // console.log(value[2])
+  });
+}
+
+//FUNCTIONS
 function getUserName() {
   let newId = getRandomUserId(globalUserData);
   let newUser = globalUserRepository.getUserDataBasedOnId(newId);
@@ -84,14 +71,15 @@ function getUserName() {
   welcomeText.innerText = `Welcome, ${newUserFirstName}!`;
   displayIdCardInfo(newUser);
   displayStepsInfo(newUser);
-  displayHydrationInfo(newUser)
+  displayHydrationInfo(newUser);
 }
+
 function displayHydrationInfo(newUser) {
   let hydrationId = globalHydration.obtainHydrationDataBasedOnId(newUser.id);
   let hydrationDaily = globalHydration.obtainOuncesForMostRecentDay(hydrationId);
   let hydrationWeekly = globalHydration.obtainOuncesPerDayOverAWeek(hydrationId);
-  todaysHydration.innerText += `${hydrationDaily}`
-  weeklyHydration.innerText += `${hydrationWeekly}`
+  todaysHydration.innerText += ` ${hydrationDaily}`;
+  weeklyHydration.innerText += ` ${hydrationWeekly}`;
 }
 
 function displayIdCardInfo(newUser) {
@@ -110,11 +98,6 @@ function displayStepsInfo(newUser) {
   averageUsersStepGoal.innerText += ` ${aveStepGoal}`;
 }
 
-function getRandomUserId(anyUserDataArr) {
-  return anyUserDataArr[Math.floor(Math.random() * anyUserDataArr.length)].id;
+function getRandomUserId(anyUserData) {
+  return anyUserData[Math.floor(Math.random()*anyUserData.length)].id;
 }
-
-// An example of how you tell webpack to use a JS file
-// import userData from './data/users';
-import UserRepository from './UserRepository';
-import Hydration from './Hydration';
