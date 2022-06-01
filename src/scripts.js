1a06446fcd2b954057cb3f7069f412b80eff3539
Irmak Erdem <irmakerdem9@gmail.com>
2022-06-01 15:59:41 -0600

    Refactor displaySleepInfo function into 6 individual functions for SRP

--- src/scripts.js
+++ src/scripts.js
@@ -86,35 +86,50 @@ function getUserName() {
   displaySleepInfo(newUser);
 }
 
+///////////////////////////////////////////////////////////////////
 function displaySleepInfo(newUser) {
   let sleepId = globalSleep.acquireSleepDataBasedOnId(newUser.id);
-  let hoursArr = sleepId.map(daySleep => {
-    return daySleep.hoursSlept
-  })
+  let finalIndexDate = dateEdit(sleepId);
+  displayAveHours(sleepId);
+  displayAveQuality(sleepId);
+  specificDayStuff(sleepId, finalIndexDate);
+  weekStuff(sleepId, finalIndexDate);
+}
 
-  let averageHoursDom = globalSleep.acquireAverageMetricPerDay(hoursArr);
-  averageHours.innerText += ` ${averageHoursDom}`;
-
-  let qualityArr = sleepId.map(daySleep => {
-    return daySleep.sleepQuality
-  })
-
-  let averageQualityDom = globalSleep.acquireAverageMetricPerDay(qualityArr);
-  console.log('avgquality', averageQualityDom)
-  averageQuality.innerText += ` ${averageQualityDom}`;
+function dateEdit(sleepId) {
   let lastIndexNum = [sleepId.length - 1];
   let indexOfBigData = (sleepId[lastIndexNum]);
-  let finalIndexDate = indexOfBigData.date;
+  return indexOfBigData.date;
+}
+
+function displayAveHours(sleepId) {
+  let hoursArr = sleepId.map(daySleep => {
+    return daySleep.hoursSlept;
+  })
+  let averageHoursDom = globalSleep.acquireAverageMetricPerDay(hoursArr);
+  averageHours.innerText += ` ${averageHoursDom}`;
+}
+
+function displayAveQuality(sleepId) {
+  let qualityArr = sleepId.map(daySleep => {
+    return daySleep.sleepQuality;
+  })
+  let averageQualityDom = globalSleep.acquireAverageMetricPerDay(qualityArr);
+  averageQuality.innerText += ` ${averageQualityDom}`;
+}
+
+function specificDayStuff(sleepId, finalIndexDate) {
   let todaysHoursDom = globalSleep.acquireHoursSleptForASpecificDay(sleepId, finalIndexDate);
   todaysHours.innerText += ` ${todaysHoursDom}`;
   let todaysQualityDom = globalSleep.acquireSleepQualityForASpecificDay(sleepId, finalIndexDate);
   todaysQuality.innerText += ` ${todaysQualityDom}`;
+}
 
+function weekStuff(sleepId, finalIndexDate) {
   let lastWeekHoursDom = globalSleep.acquireHoursSleptEachDayForAWeek(sleepId, finalIndexDate);
   var allFixedDisplayObjects = good(lastWeekHoursDom, 'Hours: ')
   let noCommas = allFixedDisplayObjects.join("<br />");
   lastWeeksHours.innerHTML += `<br> ${noCommas}`
-
   let lastWeekQualityDom = globalSleep.acquireSleepQualityEachDayForAWeek(sleepId, finalIndexDate);
   var sleepObjectsDom = good(lastWeekQualityDom, 'Quality: ')
   let noCommas2 = sleepObjectsDom.join("<br />");
@@ -146,27 +161,10 @@ function displayHydrationInfo(newUser) {
   let hydrationDaily = globalHydration.obtainOuncesForMostRecentDay(hydrationId);
   let hydrationWeekly = globalHydration.obtainOuncesPerDayOverAWeek(hydrationId);
   todaysHydration.innerText += ` ${hydrationDaily} ounces`;
-  var hydrationObjectsDom = good(hydrationWeekly, 'Ounces: ')
+  var hydrationObjectsDom = good(hydrationWeekly, 'Ounces: ');
   let noCommas3 = hydrationObjectsDom.join("<br />");
   weeklyHydration.innerHTML += `<br>${noCommas3}`;
 }
-
-// function displayIdCardInfo(newUser) {
-//   idText.innerText += ` ${newUser.id}`;
-//   nameText.innerText += ` ${newUser.name}`;
-//   addressText.innerText += ` ${newUser.address}`;
-//   emailText.innerText += ` ${newUser.email}`;
-//   strideLengthText.innerText += ` ${newUser.strideLength}`;
-//   dailyStepGoalText.innerText += ` ${newUser.dailyStepGoal}`;
-//   var userFriend = newUser.friends.map(friend => {
-//     return friend = globalUserRepository.getUserDataBasedOnId(friend)
-//   });
-//   let userFriendNames = userFriend.map(friend => {
-//     return friend.returnUserFirstName()
-//   });
-//   let friendsSpace = userFriendNames.join(", ");
-//   friendsText.innerText += '\xa0' + friendsSpace;
-// }
 
 function displayIdCardInfo(newUser) {
   idText.innerText += ` ${newUser.id}`;
@@ -176,9 +174,9 @@ function displayIdCardInfo(newUser) {
   strideLengthText.innerText += ` ${newUser.strideLength}`;
   dailyStepGoalText.innerText += ` ${newUser.dailyStepGoal}`;
   var userFriend = newUser.friends.map(friend => {
-    let foo = globalUserRepository.getUserDataBasedOnId(friend)
-    let bar = foo.returnUserFirstName()
-    return bar
+    let foo = globalUserRepository.getUserDataBasedOnId(friend);
+    let bar = foo.returnUserFirstName();
+    return bar;
   });
   let friendsSpace = userFriend.join(", ");
   friendsText.innerText += '\xa0' + friendsSpace;
